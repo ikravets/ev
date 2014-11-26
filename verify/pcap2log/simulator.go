196d5b451c5f2062075c912651b035932dfc1dad
Ilia Kravets <ilia.kravets@gmail.com>
2014-11-26 10:47:32 +0200

    pcap2log: improve printing

--- verify/pcap2log/simulator.go
+++ verify/pcap2log/simulator.go
@@ -10,6 +10,8 @@ import (
 	"log"
 )
 
+var _ = pretty.Print
+
 type simulator struct {
 	w io.Writer
 }
@@ -24,7 +26,24 @@ func (s *simulator) addMessage(qom *QOMessage, typeChar byte) {
 	s.outMessageNorm(qom, typeChar)
 }
 
+func (s *simulator) Printf(format string, vs ...interface{}) {
+	if _, err := fmt.Fprintf(s.w, format, vs...); err != nil {
+		log.Fatal("output error", err)
+	}
+}
+
+func (s *simulator) Printfln(format string, vs ...interface{}) {
+	s.Printf(format, vs...)
+	if _, err := fmt.Fprintln(s.w); err != nil {
+		log.Fatal("output error", err)
+	}
+}
+
 func (s *simulator) outMessageNorm(m *QOMessage, typeChar byte) {
+	out := func(name, f string, vs ...interface{}) {
+		s.Printf("NORM %s %c ", name, typeChar)
+		s.Printfln(f, vs...)
+	}
 	ord, bid, ask := &m.side1, &m.side1, &m.side2
 	if bid.side == MarketSideSell {
 		bid, ask = ask, bid
@@ -32,40 +51,29 @@ func (s *simulator) outMessageNorm(m *QOMessage, typeChar byte) {
 	switch m.typ {
 	case MessageTypeUnknown: // ignore
 	case MessageTypeQuoteAdd:
-		fmt.Fprintf(s.w, "%s %c %08x %08x %08x %08x\n",
-			"NORM QBID", typeChar, m.optionId, bid.refNumDelta, bid.size, bid.price)
-		fmt.Fprintf(s.w, "%s %c %08x %08x %08x %08x\n",
-			"NORM QASK", typeChar, m.optionId, ask.refNumDelta, ask.size, ask.price)
+		out("QBID", "%08x %08x %08x %08x", m.optionId, bid.refNumDelta, bid.size, bid.price)
+		out("QASK", "%08x %08x %08x %08x", m.optionId, ask.refNumDelta, ask.size, ask.price)
 	case MessageTypeQuoteReplace:
-		fmt.Fprintf(s.w, "%s %c %08x %08x %08x %08x\n",
-			"NORM QBID", typeChar, bid.refNumDelta, bid.origRefNumDelta, bid.size, bid.price)
-		fmt.Fprintf(s.w, "%s %c %08x %08x %08x %08x\n",
-			"NORM QASK", typeChar, ask.refNumDelta, ask.origRefNumDelta, ask.size, ask.price)
+		out("QBID", "%08x %08x %08x %08x", bid.refNumDelta, bid.origRefNumDelta, bid.size, bid.price)
+		out("QASK", "%08x %08x %08x %08x", ask.refNumDelta, ask.origRefNumDelta, ask.size, ask.price)
 	case MessageTypeQuoteDelete:
-		fmt.Fprintf(s.w, "%s %c %08x\n",
-			"NORM QBID", typeChar, bid.origRefNumDelta)
-		fmt.Fprintf(s.w, "%s %c %08x\n",
-			"NORM QASK", typeChar, ask.origRefNumDelta)
+		out("QBID", "%08x", bid.origRefNumDelta)
+		out("QASK", "%08x", ask.origRefNumDelta)
 	case MessageTypeOrderAdd:
-		fmt.Fprintf(s.w, "%s %c %c %08x %08x %08x %08x\n",
-			"NORM ORDER", typeChar, ord.side, m.optionId, ord.refNumDelta, ord.size, ord.price)
+		out("ORDER", "%c %08x %08x %08x %08x", ord.side, m.optionId, ord.refNumDelta, ord.size, ord.price)
 	case MessageTypeOrderExecute, MessageTypeOrderExecuteWPrice, MessageTypeOrderCancel:
-		fmt.Fprintf(s.w, "%s %c %08x %08x\n",
-			"NORM ORDER", typeChar, ord.origRefNumDelta, ord.size)
+		out("ORDER", "%08x %08x", ord.origRefNumDelta, ord.size)
 	case MessageTypeOrderUpdate:
-		fmt.Fprintf(s.w, "%s %c %08x %08x %08x\n",
-			"NORM ORDER", typeChar, ord.origRefNumDelta, ord.size, ord.price)
+		out("ORDER", "%08x %08x %08x", ord.origRefNumDelta, ord.size, ord.price)
 	case MessageTypeOrderReplace:
-		fmt.Fprintf(s.w, "%s %c %08x %08x %08x %08x\n",
-			"NORM ORDER", typeChar, ord.refNumDelta, ord.origRefNumDelta, ord.size, ord.price)
+		out("ORDER", "%08x %08x %08x %08x", ord.refNumDelta, ord.origRefNumDelta, ord.size, ord.price)
 	case MessageTypeOrderDelete:
-		fmt.Fprintf(s.w, "%s %c %08x\n",
-			"NORM ORDER", typeChar, ord.origRefNumDelta)
+		out("ORDER", "%08x", ord.origRefNumDelta)
 	case MessageTypeBlockOrderDelete:
 		for _, r := range m.bssdRefs {
-			fmt.Fprintf(s.w, "%s %c %08x\n", "NORM ORDER", typeChar, r)
+			out("ORDER", "%08x", r)
 		}
 	default:
-		log.Fatalf("Unexpected message type %d\nmessage=%s\n", m.typ, pretty.Sprintf("%v", *m))
+		log.Fatalf("Unexpected message type %d in %+v\n", m.typ, m)
 	}
 }
