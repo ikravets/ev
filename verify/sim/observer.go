771b96c740f8f5fe2f72dc09a063be271d24ebac
Ilia Kravets <ilia.kravets@gmail.com>
2015-02-05 17:18:59 +0200

    add sim.MuxObserver

--- verify/sim/observer.go
+++ verify/sim/observer.go
@@ -16,3 +16,31 @@ func (*NilObserver) MessageArrived(*IttoDbMessage)          {}
 func (*NilObserver) OperationAppliedToOrders(IttoOperation) {}
 func (*NilObserver) BeforeBookUpdate(Book, IttoOperation)   {}
 func (*NilObserver) AfterBookUpdate(Book, IttoOperation)    {}
+
+type MuxObserver struct {
+	slaves []Observer
+}
+
+func (mo *MuxObserver) AppendSlave(slave Observer) {
+	mo.slaves = append(mo.slaves, slave)
+}
+func (mo *MuxObserver) MessageArrived(m *IttoDbMessage) {
+	for _, slave := range mo.slaves {
+		slave.MessageArrived(m)
+	}
+}
+func (mo *MuxObserver) OperationAppliedToOrders(o IttoOperation) {
+	for _, slave := range mo.slaves {
+		slave.OperationAppliedToOrders(o)
+	}
+}
+func (mo *MuxObserver) BeforeBookUpdate(b Book, o IttoOperation) {
+	for _, slave := range mo.slaves {
+		slave.BeforeBookUpdate(b, o)
+	}
+}
+func (mo *MuxObserver) AfterBookUpdate(b Book, o IttoOperation) {
+	for _, slave := range mo.slaves {
+		slave.AfterBookUpdate(b, o)
+	}
+}
