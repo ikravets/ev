fcf9fc49ee968df082cd3106bf9f81c1f335b0eb
Ilia Kravets <ilia.kravets@gmail.com>
2015-02-08 23:38:30 +0200

    cosmetic changes: rename pcap2txt -> cmdPcap2txt

--- verify/cmd/pcap2txt.go
+++ verify/cmd/pcap2txt.go
@@ -16,34 +16,31 @@ import (
 	"my/itto/verify/packet"
 )
 
-type pcap2txt struct {
+type cmdPcap2txt struct {
 	InputFileName  string `long:"input" short:"i" required:"y" value-name:"PCAP_FILE" description:"input pcap file to read"`
 	OutputFileName string `long:"output" short:"o" value-name:"FILE" default:"/dev/stdout" default-mask:"stdout" description:"output file"`
 	shouldExecute  bool
 }
 
-func (p *pcap2txt) Execute(args []string) error {
-	p.shouldExecute = true
+func (c *cmdPcap2txt) Execute(args []string) error {
+	c.shouldExecute = true
 	return nil
 }
 
-func (p *pcap2txt) ConfigParser(parser *flags.Parser) {
-	parser.AddCommand("pcap2txt",
-		"convert pcap file to human-readable text",
-		"",
-		p)
+func (c *cmdPcap2txt) ConfigParser(parser *flags.Parser) {
+	parser.AddCommand("pcap2txt", "convert pcap file to human-readable text", "", c)
 }
 
-func (p *pcap2txt) ParsingFinished() {
-	if !p.shouldExecute {
+func (c *cmdPcap2txt) ParsingFinished() {
+	if !c.shouldExecute {
 		return
 	}
-	handle, err := pcap.OpenOffline(p.InputFileName)
+	handle, err := pcap.OpenOffline(c.InputFileName)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer handle.Close()
-	outFile, err := os.OpenFile(p.OutputFileName, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0644)
+	outFile, err := os.OpenFile(c.OutputFileName, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0644)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -57,7 +54,7 @@ func (p *pcap2txt) ParsingFinished() {
 }
 
 func init() {
-	var c pcap2txt
+	var c cmdPcap2txt
 	Registry.Register(&c)
 }
 
