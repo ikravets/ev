2575325ff75778f5cdd9555d007a7a0c69a9be84
Ilia Kravets <ilia.kravets@gmail.com>
2015-02-18 15:31:53 +0200

    efhsim: add AvtLogger, new output format

--- verify/cmd/efhsim.go
+++ verify/cmd/efhsim.go
@@ -19,6 +19,8 @@ type cmdEfhsim struct {
 	OutputFileNameSim       string `long:"output-sim" value-name:"FILE" description:"output file for hw simulator"`
 	OutputFileNameEfhOrders string `long:"output-efh-orders" value-name:"FILE" description:"output file for EFH order messages"`
 	OutputFileNameEfhQuotes string `long:"output-efh-quotes" value-name:"FILE" description:"output file for EFH quote messages"`
+	OutputFileNameAvt       string `long:"output-avt" value-name:"FILE" description:"output file for AVT CSV"`
+	InputFileNameAvtDict    string `long:"avt-dict" value-name:"DICT" description:"read dictionary for AVT CSV output"`
 	PacketNumLimit          int    `long:"count" short:"c" value-name:"NUM" description:"limit number of input packets"`
 	shouldExecute           bool
 	outFiles                []io.Closer
@@ -59,6 +61,17 @@ func (c *cmdEfhsim) ParsingFinished() {
 		logger := efhsim.NewEfhLogger(w)
 		logger.SetOutputMode(efhsim.EfhLoggerOutputQuotes)
 		return efh.AddLogger(logger)
+	})
+	c.addOut(c.OutputFileNameAvt, func(w io.Writer) (err error) {
+		var dict io.ReadCloser
+		if c.InputFileNameAvtDict != "" {
+			if dict, err = os.Open(c.InputFileNameAvtDict); err != nil {
+				return
+			} else {
+				c.outFiles = append(c.outFiles, dict)
+			}
+		}
+		return efh.AddLogger(efhsim.NewAvtLogger(w, dict))
 	})
 	if err := efh.AnalyzeInput(); err != nil {
 		log.Fatal(err)
---
+++ verify/efhsim/avt_logger.go
@@ -0,0 +1,133 @@
+// Copyright (c) Ilia Kravets, 2015. All rights reserved. PROVIDED "AS IS"
+// WITHOUT ANY WARRANTY, EXPRESS OR IMPLIED. See LICENSE file for details.
+
+package efhsim
+
+import (
+	"bytes"
+	"encoding/csv"
+	"fmt"
+	"io"
+	"log"
+	"strconv"
+	"time"
+
+	"my/itto/verify/packet/itto"
+	"my/itto/verify/sim"
+)
+
+var _ sim.Observer = &AvtLogger{}
+
+type AvtLogger struct {
+	w           io.Writer
+	location    *time.Location
+	oid2AvtName map[int]string
+	TobLogger
+}
+
+func NewAvtLogger(w io.Writer, rDict io.Reader) *AvtLogger {
+	l := &AvtLogger{
+		w:         w,
+		TobLogger: *NewTobLogger(),
+	}
+	var err error
+	if l.location, err = time.LoadLocation("EST"); err != nil {
+		log.Fatal(err)
+	}
+
+	if rDict != nil {
+		r := csv.NewReader(rDict)
+		records, err := r.ReadAll()
+		if err != nil {
+			log.Fatal(err)
+		}
+		l.oid2AvtName = make(map[int]string)
+		for _, rec := range records {
+			if len(rec) != 2 {
+				log.Fatalf("unexpected dict csv record: %#v\n", rec)
+			}
+			if rec[0] == "avtSymbol" && rec[1] == "exchangeId" {
+				continue // header
+			}
+			if oid, err := strconv.Atoi(rec[1]); err != nil {
+				log.Fatal(err)
+			} else {
+				l.oid2AvtName[oid] = rec[0]
+			}
+		}
+	}
+	return l
+}
+
+func (l *AvtLogger) AfterBookUpdate(book sim.Book, operation sim.IttoOperation) {
+	if l.TobLogger.AfterBookUpdate(book, operation, TobUpdateNewForce) {
+		l.genUpdate()
+	}
+}
+
+func (l *AvtLogger) genUpdate() {
+	var optName, underlying string
+	if l.oid2AvtName != nil {
+		optName = l.oid2AvtName[int(l.lastOptionId)]
+		bs := []byte(optName)
+		for i := range bs {
+			if bs[i] < 'A' || bs[i] > 'Z' {
+				bs = bs[:i]
+				break
+			}
+		}
+		underlying = string(bs)
+
+	}
+	if optName == "" {
+		optName = fmt.Sprintf("<%d>", l.lastOptionId)
+		underlying = "<?>"
+	}
+	packetTimestamp := l.lastMessage.Pam.PacketMetadata().Timestamp.In(l.location)
+	dateTime := packetTimestamp.Format("20060102,15:04:05.000.")
+	dateTime += fmt.Sprintf("%03d", packetTimestamp.Nanosecond()/1000%1000)
+	ittoTimestamp := uint64(l.ittoSeconds)*1e9 + uint64(l.lastMessage.Pam.Layer().(itto.IttoMessageCommon).Base().Timestamp)
+	avtTimestamp := time.Date(packetTimestamp.Year(), packetTimestamp.Month(), packetTimestamp.Day(), 0, 0, 0, 0, l.location).Add(time.Duration(ittoTimestamp)).UnixNano() / 1000000
+	if false {
+		// OptionMarketDataNASDAQ2,date,time,Security,Underlying,SecurityType,BidSize,BidPrice,OrderBidSize,AskSize,AskPrice,OrderAskSize,TradeStatus,TickCondition,ExchangeTimestamp
+		fmt.Fprintf(l.w, "OptionMarketDataNASDAQ2,%s,%s,%s,1,%d,%s,0,%d,%s,0,,,%d\n",
+			dateTime,
+			optName,
+			underlying,
+			l.bid.New.Size,
+			priceString(l.bid.New.Price),
+			l.ask.New.Size,
+			priceString(l.ask.New.Price),
+			avtTimestamp,
+		)
+	} else {
+		if underlying == "<?>" {
+			return
+		}
+		// ExchangeTimestamp, Date, Time, Security, BidSize, BidPrice, AskSize, AskPrice,
+		fmt.Fprintf(l.w, "%d,%s,%s,%d,%s,%d,%s\n",
+			avtTimestamp,
+			dateTime[0:21],
+			optName,
+			l.bid.New.Size,
+			priceString(l.bid.New.Price),
+			l.ask.New.Size,
+			priceString(l.ask.New.Price),
+		)
+	}
+}
+
+func priceString(price int) string {
+	var buf bytes.Buffer
+	buf.Write(strconv.AppendInt(nil, int64(price/10000), 10))
+	buf.WriteByte('.')
+	decimal := price % 10000
+	for denom := 1000; denom > 0; denom /= 10 {
+		d := decimal / denom % 10
+		buf.WriteByte('0' + byte(d))
+		if decimal%denom == 0 {
+			break
+		}
+	}
+	return buf.String()
+}
