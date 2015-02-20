f26f8b0bcc7edea67f002a565b3d34d8bf2d8f68
Ilia Kravets <ilia.kravets@gmail.com>
2015-02-20 19:48:24 +0200

    packet: refactor processor.ProcessAll()

--- verify/packet/processor.go
+++ verify/packet/processor.go
@@ -7,7 +7,6 @@ import (
 	"bytes"
 	"fmt"
 	"io"
-	"log"
 
 	"code.google.com/p/gopacket"
 	"code.google.com/p/gopacket/layers"
@@ -107,30 +106,14 @@ func (p *processor) ProcessAll() error {
 			break
 		}
 
-		if packet.NetworkLayer() == nil || packet.TransportLayer() == nil || packet.ApplicationLayer() == nil {
+		mu := moldudp64Layer(packet)
+		if mu == nil {
 			continue
 		}
-		var flowBufSrc, flowBufDst bytes.Buffer
-		flowBufSrc.Write(packet.NetworkLayer().NetworkFlow().Src().Raw())
-		flowBufDst.Write(packet.NetworkLayer().NetworkFlow().Dst().Raw())
-		flowBufSrc.Write(packet.TransportLayer().TransportFlow().Src().Raw())
-		flowBufDst.Write(packet.TransportLayer().TransportFlow().Dst().Raw())
-		var flow gopacket.Flow = gopacket.InvalidFlow
-		var seqNum uint64
+		seqNum := mu.SequenceNumber
+		flow := p.getFlow(packet)
 		for _, l := range packet.Layers() {
-			if mu, ok := l.(*moldudp64.MoldUDP64); ok {
-				flowBufSrc.Write(mu.Flow().Src().Raw())
-				flowBufDst.Write(mu.Flow().Dst().Raw())
-				if flow != gopacket.InvalidFlow {
-					log.Fatal("duplicate MoldUDP64 layer")
-				}
-				flow = gopacket.NewFlow(EndpointCombinedSession, flowBufSrc.Bytes(), flowBufDst.Bytes())
-				seqNum = mu.SequenceNumber
-			}
 			if itto.LayerClassItto.Contains(l.LayerType()) {
-				if flow == gopacket.InvalidFlow {
-					log.Fatal("incorrect layer order, flow == nil")
-				}
 				m := applicationMessage{
 					layer:          l,
 					flow:           flow,
@@ -143,6 +126,18 @@ func (p *processor) ProcessAll() error {
 		}
 	}
 	return nil
+}
+
+func (p *processor) getFlow(packet gopacket.Packet) gopacket.Flow {
+	mu := moldudp64Layer(packet)
+	var flowBufSrc, flowBufDst bytes.Buffer
+	flowBufSrc.Write(packet.NetworkLayer().NetworkFlow().Src().Raw())
+	flowBufDst.Write(packet.NetworkLayer().NetworkFlow().Dst().Raw())
+	flowBufSrc.Write(packet.TransportLayer().TransportFlow().Src().Raw())
+	flowBufDst.Write(packet.TransportLayer().TransportFlow().Dst().Raw())
+	flowBufSrc.Write(mu.Flow().Src().Raw())
+	flowBufDst.Write(mu.Flow().Dst().Raw())
+	return gopacket.NewFlow(EndpointCombinedSession, flowBufSrc.Bytes(), flowBufDst.Bytes())
 }
 
 func (p *processor) decodeAppLayer(packet gopacket.Packet) error {
@@ -170,6 +165,13 @@ func (p *processor) decodeAppLayer(packet gopacket.Packet) error {
 		if mb, ok := l.(*moldudp64.MoldUDP64MessageBlock); ok {
 			ittoDecoder.Decode(mb.Payload, packetBuilder)
 		}
+	}
+	return nil
+}
+
+func moldudp64Layer(packet gopacket.Packet) *moldudp64.MoldUDP64 {
+	if l := packet.Layer(moldudp64.LayerTypeMoldUDP64); l != nil {
+		return l.(*moldudp64.MoldUDP64)
 	}
 	return nil
 }
