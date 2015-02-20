d1ed37cb9c69a2f2360b6d320040cdfd45108440
Ilia Kravets <ilia.kravets@gmail.com>
2015-02-20 19:05:56 +0200

    add --profile-{cpu,mem} options

--- verify/main.go
+++ verify/main.go
@@ -8,6 +8,7 @@ import (
 	"log"
 	"my/itto/verify/cmd"
 	"os"
+	"runtime/pprof"
 
 	"github.com/jessevdk/go-flags"
 )
@@ -16,6 +17,8 @@ func processArgs() {
 	var opts struct {
 		Verbose     []bool `short:"v" long:"verbose" description:"show verbose debug information"`
 		LogFileName string `short:"l" long:"log" value-name:"FILE" default:"/dev/stderr" default-mask:"stderr" description:"log file"`
+		ProfileCpu  string `long:"profile-cpu" value-name:"FILE"`
+		ProfileMem  string `long:"profile-mem" value-name:"FILE"`
 	}
 
 	parser := flags.NewParser(&opts, flags.PassDoubleDash|flags.HelpFlag|flags.IgnoreUnknown)
@@ -24,13 +27,33 @@ func processArgs() {
 		fmt.Println(err.(*flags.Error).Message)
 		os.Exit(1)
 	}
+
 	logFile, err := os.OpenFile(opts.LogFileName, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
 	if err != nil {
 		log.Fatal(err)
 	}
 	log.SetOutput(logFile)
 	defer logFile.Close()
+
+	if opts.ProfileCpu != "" {
+		profFile, err := os.Create(opts.ProfileCpu)
+		if err != nil {
+			log.Fatal(err)
+		}
+		pprof.StartCPUProfile(profFile)
+		defer pprof.StopCPUProfile()
+	}
+
 	cmd.Registry.ParsingFinished()
+
+	if opts.ProfileMem != "" {
+		profFile, err := os.Create(opts.ProfileMem)
+		if err != nil {
+			log.Fatal(err)
+		}
+		pprof.WriteHeapProfile(profFile)
+		profFile.Close()
+	}
 }
 
 func main() {
