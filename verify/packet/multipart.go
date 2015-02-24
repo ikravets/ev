ce05c9c3074fc724654f8a75dfb396b6c1e9d9ce
Ilia Kravets <ilia.kravets@gmail.com>
2015-02-24 10:27:58 +0200

    packet: add DecodingMultiLayer interface

---
+++ verify/packet/multipart.go
@@ -0,0 +1,19 @@
+// Copyright (c) Ilia Kravets, 2015. All rights reserved. PROVIDED "AS IS"
+// WITHOUT ANY WARRANTY, EXPRESS OR IMPLIED. See LICENSE file for details.
+
+package packet
+
+import "code.google.com/p/gopacket"
+
+type DecodingMultiLayer interface {
+	//similarly to gopacket.DecodingLayer
+	DecodeFromBytes(data []byte, df gopacket.DecodeFeedback) error
+	CanDecode() gopacket.LayerClass
+
+	NextLayers() []TypedPayload
+}
+
+type TypedPayload struct {
+	Type    gopacket.LayerType
+	Payload []byte
+}
