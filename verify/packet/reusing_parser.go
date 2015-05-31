c4a274e840309fd0eb473cf544342a3b12d4d17d
Ilia Kravets <ilia.kravets@gmail.com>
2015-05-31 17:48:33 +0300

    packet: update ReusingLayerParser

--- verify/packet/reusing_parser.go
+++ verify/packet/reusing_parser.go
@@ -3,7 +3,13 @@
 
 package packet
 
-import "code.google.com/p/gopacket"
+import (
+	"log"
+
+	"code.google.com/p/gopacket"
+
+	"my/errs"
+)
 
 type DecodingLayerFactory interface {
 	Create(gopacket.LayerType) gopacket.DecodingLayer
@@ -40,19 +46,25 @@ func (p *ReusingLayerParser) AddDecodingLayerFactory(f DecodingLayerFactory) {
 }
 
 func (p *ReusingLayerParser) DecodeLayers(data []byte, decoded *[]gopacket.DecodingLayer) (err error) {
+	errs.PassE(&err)
 	p.recycle(*decoded)
 	*decoded = (*decoded)[:0]
 	p.Truncated = false
-	typ := p.first
-	for len(data) > 0 {
-		if layer, err := p.getDecodingLayer(typ); err != nil {
-			return err
-		} else if err = layer.DecodeFromBytes(data, p.df); err != nil {
-			return err
-		} else {
-			*decoded = append(*decoded, layer)
-			typ = layer.NextLayerType()
-			data = layer.LayerPayload()
+
+	tps := []TypedPayload{TypedPayload{Type: p.first, Payload: data}}
+	for len(tps) > 0 {
+		tp := tps[0]
+		tps = tps[1:]
+		layer, err := p.getDecodingLayer(tp.Type)
+		errs.CheckE(err)
+		//errs.CheckE(layer.DecodeFromBytes(tp.Payload, p.df))
+		layer.DecodeFromBytes(tp.Payload, p.df) // FIXME check returned error
+		*decoded = append(*decoded, layer)
+
+		if dml, ok := layer.(DecodingMultiLayer); ok {
+			tps = append(tps, dml.NextLayers()...)
+		} else if len(layer.LayerPayload()) > 0 {
+			tps = append(tps, TypedPayload{Type: layer.NextLayerType(), Payload: layer.LayerPayload()})
 		}
 	}
 	return nil
@@ -61,10 +73,7 @@ func (p *ReusingLayerParser) DecodeLayers(data []byte, decoded *[]gopacket.Decod
 func (p *ReusingLayerParser) recycle(layers []gopacket.DecodingLayer) {
 	for _, layer := range layers {
 		typ := layer.CanDecode().LayerTypes()[0]
-		if cachedLayers, ok := p.layerCache[typ]; ok {
-			cachedLayers = append(cachedLayers, layer)
-			p.layerCache[typ] = cachedLayers
-		}
+		p.layerCache[typ] = append(p.layerCache[typ], layer)
 	}
 }
 
@@ -86,3 +95,5 @@ func (p *ReusingLayerParser) getDecodingLayer(typ gopacket.LayerType) (layer gop
 func (p *ReusingLayerParser) SetTruncated() {
 	p.Truncated = true
 }
+
+var _ = log.Ldate
