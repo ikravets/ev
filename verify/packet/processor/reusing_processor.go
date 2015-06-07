6cf83870793f6779a5696d24ebfd4d0d21e7d314
Ilia Kravets <ilia.kravets@gmail.com>
2015-06-07 16:08:54 +0300

    packet/processor: add BATS support

--- verify/packet/processor/reusing_processor.go
+++ verify/packet/processor/reusing_processor.go
@@ -16,6 +16,7 @@ import (
 	"my/errs"
 
 	"my/itto/verify/packet"
+	"my/itto/verify/packet/bats"
 	"my/itto/verify/packet/miax"
 )
 
@@ -46,6 +47,9 @@ func (p *reusingProcessor) LimitPacketNumber(limit int) {
 func (p *reusingProcessor) ProcessAll() (err error) {
 	errs.PassE(&err)
 	pd := NewEndpointPayloadDetector()
+	for port := 30100; port < 30200; port++ {
+		pd.addDstMap(layers.NewUDPPortEndpoint(layers.UDPPort(port)), bats.LayerTypeBSU)
+	}
 	for port := 51000; port < 51100; port++ {
 		pd.addDstMap(layers.NewUDPPortEndpoint(layers.UDPPort(port)), miax.LayerTypeMachTop)
 	}
@@ -62,6 +66,8 @@ func (p *reusingProcessor) ProcessAll() (err error) {
 	parser.AddDecodingLayerFactory(miax.MachTopLayerFactory)
 	parser.AddDecodingLayerFactory(miax.MachLayerFactory)
 	parser.AddDecodingLayerFactory(miax.TomLayerFactory)
+	parser.AddDecodingLayerFactory(bats.BSULayerFactory)
+	parser.AddDecodingLayerFactory(bats.PitchLayerFactory)
 
 	packetNumLimit := -1
 	if p.packetNumLimit > 0 {
@@ -111,6 +117,18 @@ func (p *reusingProcessor) ProcessPacket(data []byte, ci gopacket.CaptureInfo, d
 				timestamp: pkt.Timestamp(),
 			}
 			p.handler.HandleMessage(&m)
+		case *bats.BSU:
+			flow = gopacket.NewFlow(packet.EndpointCombinedSession, p.flowBufSrc.Bytes(), p.flowBufDst.Bytes())
+			seqNum = uint64(l.Sequence)
+		case bats.PitchMessage:
+			m := applicationMessage{
+				layer:     l,
+				flow:      flow,
+				seqNum:    seqNum,
+				timestamp: pkt.Timestamp(),
+			}
+			p.handler.HandleMessage(&m)
+			seqNum++
 		}
 	}
 	return
