e4699ef4382b6a66d405f3cb76a6ab180528f1b7
Ilia Kravets <ilia.kravets@gmail.com>
2015-07-30 05:52:22 +0300

    cmd: add echo command

---
+++ src/my/ev/cmd/echo.go
@@ -0,0 +1,36 @@
+// Copyright (c) Ilia Kravets, 2015. All rights reserved. PROVIDED "AS IS"
+// WITHOUT ANY WARRANTY, EXPRESS OR IMPLIED. See LICENSE file for details.
+
+package cmd
+
+import (
+	"github.com/jessevdk/go-flags"
+
+	"my/ev/echo"
+)
+
+type cmdEcho struct {
+	shouldExecute bool
+}
+
+func (c *cmdEcho) Execute(args []string) error {
+	c.shouldExecute = true
+	return nil
+}
+
+func (c *cmdEcho) ConfigParser(parser *flags.Parser) {
+	parser.AddCommand("echo", "run echo server", "", c)
+}
+
+func (c *cmdEcho) ParsingFinished() {
+	if !c.shouldExecute {
+		return
+	}
+	es := echo.NewEchoServer()
+	es.Run()
+}
+
+func init() {
+	var c cmdEcho
+	Registry.Register(&c)
+}
